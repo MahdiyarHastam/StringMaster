f8dc90585f9a8a99cbacbd3aa5a6e6e0472c9ab6
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-02-29 22:46:56 +0330

    soal charom

---
+++ Answers/40230112057/src/Problem5.java
@@ -0,0 +1,13 @@
+public class Problem5 {
+    public static String normalizingName(String firstName, String lastName){
+        String First=firstName.toLowerCase();
+        String Second=lastName.toLowerCase();
+        char[] neww1=First.toCharArray();
+        char[] neww2=Second.toCharArray();
+        neww1[0]-=32;
+        neww2[0]-=32;
+        String akhari=new String(neww1);
+        String akharii=new String(neww2);
+        return akhari+" "+akharii;
+    }
+}
