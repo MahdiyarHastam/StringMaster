d820b72af520f9810fffb350c465992ec6199cd0
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-03-02 10:44:28 +0330

    final touch

---
+++ Answers/40230112057/src/main/java/Advanced.java
@@ -0,0 +1,67 @@
+import java.util.Objects;
+
+public class Advanced {
+
+    /**
+     * Goal : Changing a Sentence Content
+
+     * In this function, you have a sentence, a word & a newWord as Entry
+     * You have to search the sentence to find the word that you were given as input and change it with the newWord
+
+     */
+    public String wordCensor(String sentence, String word, String newWord){
+        String[] splitt=sentence.split(" ");
+        for(int i=0;i<splitt.length;i++){
+            if(splitt[i].equals(word)==true){
+                splitt[i]=newWord;
+            }
+        }
+        String neww=new String();
+        for(int i=0;i<splitt.length;i++){
+            neww=neww.concat(splitt[i]+" ");
+        }
+        return neww;
+    }
+
+    /**
+     * In this function You have a firstName and a lastName as Entry and you have to normalize them as a fullName
+     * @param firstName is a first name with irregular letters (example : hARry)
+     * @param lastName is a last name with irregular letters (example : pOtTeR)
+     * @return fullName is a normal full name that just the first letter of firstName & lastName is Capitalized (example : Harry Potter)
+     */
+    public String normalizingName(String firstName, String lastName){
+        String First=firstName.toLowerCase();
+        String Second=lastName.toLowerCase();
+        char[] neww1=First.toCharArray();
+        char[] neww2=Second.toCharArray();
+        neww1[0]-=32;
+        neww2[0]-=32;
+        String akhari=new String(neww1);
+        String akharii=new String(neww2);
+        return akhari+" "+akharii;
+    }
+
+    /**
+     * Removing repeated letter in a word
+     * @param word This input could have Consecutive repeated letters or not
+     * @return if word contains Consecutive repeated letters, one of the repeated letters should be omitted
+     */
+    public String doubleChar(String word) {
+        char[] Word=word.toCharArray();
+        String finall="";
+        int j=0;
+        for(int i=0;i<Word.length;){
+            if(Word[i]==Word[j]){
+                i++;
+            }
+            else if(Word[i]!=Word[j]){
+                finall+=Word[j];
+                j=i;
+                i++;
+            }
+        }
+        finall+=Word[Word.length-1];
+        return finall;
+    }
+}
+
---
+++ Answers/40230112057/src/main/java/Warmup.java
@@ -0,0 +1,72 @@
+public class Warmup {
+
+    /**
+     * Goal : Simple Introduction To Strings
+     * In the first function, your inputs are a number and a sentence
+     * @return is the number th word of the sentence
+     */
+    public String wordFinder(String sentence, int number) {
+        String[] ok=sentence.split(" ");
+        String number1=Integer.toString(number);
+        if(number>ok.length)
+            return "Number = "+number1+" is out Of Bound";
+        return ok[n-1];
+    }
+
+    /**
+     * Goal : Basic introduction to Strings & using foreach
+     * @param number        is in String type
+     * @param searchForEven is a boolean entry
+     * @return if searchForEven is true ? return the number of even numbers : return the number of odd numbers
+     */
+    public int oddEvenCounter(String number, boolean searchForEven) {
+        int count=0;
+        long adad=Long.parseLong(number);
+        if(searchForEven==true){
+            while(adad>0){
+                if(adad%2==0)
+                    count++;
+                adad=adad/10;
+            }
+        }
+        else{
+            while(adad>0){
+                if(adad%2==1)
+                    count++;
+                adad=adad/10;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * @param wordA --> first word
+     * @param wordB --> second word
+     * @return The word that is first in alphabet column
+     */
+    public String firstWord(String wordA, String wordB) {
+        char[] word1=wordA.toCharArray();
+        char[] word2=wordB.toCharArray();
+        if(word1.length>word2.length){
+            for(int i=0;i<word2.length;i++){
+                if(word2[i]>word1[i]){
+                    return wordA;
+                }
+                if(word2[i]<word1[i]){
+                    return wordB;
+                }
+            }
+        }
+        else{
+            for(int i=0;i<word1.length;i++){
+                if(word2[i]>word1[i]){
+                    return wordA;
+                }
+                if(word2[i]<word1[i]){
+                    return wordB;
+                }
+            }
+        }
+        return "words are equal";
+    }
+}
