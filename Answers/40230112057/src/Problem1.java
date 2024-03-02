26828b9954e37c149dc9ed2225aab80afbf0d0bf
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-03-02 10:46:27 +0330

    delete some files

--- Answers/40230112057/src/Problem1.java
+++
@@ -1,9 +0,0 @@
-public class Problem1 {
-    public static String WordFinder(String org,int n){
-        String[] ok=org.split(" ");
-        String number=Integer.toString(n);
-        if(n>ok.length)
-            return "Number = "+number+" is out Of Bound";
-        return ok[n-1];
-    }
-}
